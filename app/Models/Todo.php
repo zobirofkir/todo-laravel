4c40d723fc5b38f62cfe48241892b64bcf628e6a
csw <zobirofkir19@gmail.com>
2025-09-30 03:10:01 +0100

    add relation sheep between user and todo

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Todo extends Model
 {
@@ -11,4 +12,9 @@ class Todo extends Model
         "title",
         "content"
     ];
+
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
@@ -47,8 +48,19 @@ class User extends Authenticatable
         ];
     }
 
+    /**
+     * create passport access token
+     */
     public function accessToken() 
     {
         return $this->createToken('accessToken')->accessToken;
+    }
+
+    /**
+     * Relation Sheep between user and todo
+     */
+    public function todos() : HasMany
+    {
+        return $this->hasMany(Todo::class);
     }
 }
