c337cd3beed222334f09b003a0b698622243d019
csw <zobirofkir19@gmail.com>
2025-09-30 03:05:52 +0100

    create todo request and resource and controller

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function() {
     Route::post('register', [AuthController::class , 'register']);
     Route::post('login', [AuthController::class , 'login']);
+
+    Route::middleware('auth:api')->group(function() {
+        Route::apiResource('todos' , TodoController::class);
+    });
 });
