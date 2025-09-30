696bcc28754dafe27c3be45c6f34befb799d13fc
csw <zobirofkir19@gmail.com>
2025-09-30 03:08:31 +0100

    create todo model and migration

--- database/migrations/2025_09_30_020608_create_todos_table.php
+++ database/migrations/2025_09_30_020608_create_todos_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("user_id")->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string("title");
+            $table->longText("content");
             $table->timestamps();
         });
     }
