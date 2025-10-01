1ed67db33128b0dd715ec66a05ce9937f910925d
csw <zobirofkir19@gmail.com>
2025-10-01 01:16:58 +0100

    create todo crud

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoRequest;
+use App\Http\Resources\TodoResource;
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
@@ -11,38 +14,45 @@ class TodoController extends Controller
      */
     public function index()
     {
-        //
+        return TodoResource::collection(
+            Todo::orderBy('created_at', 'desc')->get()
+        );
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
-        //
+        return TodoResource::make(
+            Todo::create($request->validated())
+        );
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Todo $todo)
     {
-        //
+        return TodoResource::make($todo);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(TodoRequest $request, Todo $todo)
     {
-        //
+        $todo->update($request->validated());
+        return TodoResource::make(
+            $todo->refresh()
+        );
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Todo $todo)
     {
-        //
+        return $todo->delete();
     }
 }
